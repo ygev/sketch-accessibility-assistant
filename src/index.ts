32a3bf512da8789cdac8bc11502e83dccff349a8
Yana <yanacommit@wizwar.net>
2020-07-29 21:47:40 -0700

    Hello world can be seen.

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,18 @@ const helloWorld: RuleDefinition = {
   rule: async (context) => {
     context.utils.report('Hello world')
   },
-  name: 'sketch-assistant-template/hello-world',
+  name: 'sketch-color-contrast/hello-world',
   title: 'Hello World',
   description: 'Reports a hello world message',
 }
 
 const assistant: AssistantPackage = async () => {
   return {
-    name: 'sketch-assistant-template',
+    name: 'sketch-color-contrast',
     rules: [helloWorld],
     config: {
       rules: {
-        'sketch-assistant-template/hello-world': { active: true },
+        'sketch-color-contrast/hello-world': { active: true },
       },
     },
   }
