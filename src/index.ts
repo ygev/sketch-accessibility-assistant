cec6f778e07573e3b3dfe3c4abdbbccb593b9c3e
Yana <yanacommit@wizwar.net>
2020-07-29 21:54:35 -0700

    Add the initial Iterate/Test/Report norem-ipsum

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,32 @@
 import { AssistantPackage, RuleDefinition } from '@sketch-hq/sketch-assistant-types'
 
-const helloWorld: RuleDefinition = {
+const textNoLoremIpsum: RuleDefinition = {
   rule: async (context) => {
-    context.utils.report('Hello world')
+    const { utils } = context
+    // Iterate
+    for (const layer of utils.objects.text) {
+      const value = layer.attributedString.string
+      // Test
+      if (value.toLowerCase().includes('lorem ipsum')) {
+        // Report
+        utils.report(`Layer “${layer.name}” contains “lorem ipsum”`, layer)
+      }
+    }
   },
-  name: 'sketch-color-contrast/hello-world',
-  title: 'Hello World',
-  description: 'Reports a hello world message',
+  name: 'sketch-color-contrast/text-no-lorem-ipsum',
+  title: 'Text should not contain lorem ipsum',
+  description: 'Reports a violation when text layers contain lorem ipsum placeholder',
 }
 
 const assistant: AssistantPackage = async () => {
   return {
     name: 'sketch-color-contrast',
-    rules: [helloWorld],
+    rules: [textNoLoremIpsum],
     config: {
       rules: {
-        'sketch-color-contrast/hello-world': { active: true },
+        'sketch-color-contrast/text-no-lorem-ipsum': {
+          active: true,
+        },
       },
     },
   }
