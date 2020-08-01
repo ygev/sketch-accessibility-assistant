89c2cc19e146d133b80c721aaa3508631f13b32c
Yana <yanacommit@wizwar.net>
2020-08-01 16:12:34 -0700

    AA Color Contrast checker works.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
 import { AssistantPackage, RuleDefinition } from '@sketch-hq/sketch-assistant-types'
 
+// Calculating Luminance from r g b values
+function calcLum(r: number, g: number, b: number) {
+  var r_lum = r <= 0.03928 ? r / 12.92 : Math.pow((r + 0.055) / 1.055, 2.4)
+  var g_lum = g <= 0.03928 ? g / 12.92 : Math.pow((g + 0.055) / 1.055, 2.4)
+  var b_lum = b <= 0.03928 ? b / 12.92 : Math.pow((b + 0.055) / 1.055, 2.4)
+  var luminance = 0.2126 * r_lum + 0.7152 * g_lum + 0.0722 * b_lum
+  return luminance
+}
+
+// Calculating Ratio
+function calcContrast(t: number, b: number) {
+  return (Math.max(t, b) + 0.05) / (Math.min(t, b) + 0.05)
+}
+
 const textNoLoremIpsum: RuleDefinition = {
   rule: async (context) => {
     const { utils } = context
@@ -8,10 +22,42 @@ const textNoLoremIpsum: RuleDefinition = {
         if (group.layers[0]._class == 'text' && group.layers[1]._class == 'text') {
           continue
         } else {
-          utils.report(
-            `Here are the layers:“${group.name}. Layer0 Color: ${group.layers[0].style?.fills?.[0]?.color?.red}, Layer1 Color: ${group.layers[1].style?.textStyle?.encodedAttributes.MSAttributedStringColorAttribute?.red}”`,
-            group,
-          )
+          // If Shape vs. Text
+          var topR
+          var topG
+          var topB
+
+          if (group.layers[1].style?.textStyle?.encodedAttributes.MSAttributedStringColorAttribute?.red !== undefined) {
+            topR = group.layers[1].style?.textStyle?.encodedAttributes.MSAttributedStringColorAttribute?.red
+            topG = group.layers[1].style?.textStyle?.encodedAttributes.MSAttributedStringColorAttribute?.green
+            topB = group.layers[1].style?.textStyle?.encodedAttributes.MSAttributedStringColorAttribute?.blue
+          } else if (group.layers[1].style?.fills?.[0]?.color?.red !== undefined) {
+            topR = group.layers[1].style?.fills?.[0]?.color?.red
+            topG = group.layers[1].style?.fills?.[0]?.color?.green
+            topB = group.layers[1].style?.fills?.[0]?.color?.blue
+          }
+
+          var botR = group.layers[0].style?.fills?.[0]?.color?.red
+          var botG = group.layers[0].style?.fills?.[0]?.color?.green
+          var botB = group.layers[0].style?.fills?.[0]?.color?.blue
+
+          if (topR == undefined || topG == undefined || topB == undefined || botR == undefined || botG == undefined || botB == undefined) {
+            continue
+          }
+
+          // Calculate Luminosity for Text RGB and Shape RGB
+          var topLum = calcLum(topR, topG, topB)
+          var botLum = calcLum(botR, botG, botB)
+
+          var ratio = calcContrast(topLum, botLum)
+
+          if (ratio >= 4.5) {
+            // normal-size AA pass
+            continue
+          } else {
+            // normal-size AA fail
+            utils.report(`These elements do not pass WCAG 2.1 AAe, because the contrast ratio is: ${ratio.toFixed(2)}:1`, group)
+          }
         }
       }
     }
