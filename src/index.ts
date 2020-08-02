8b9f16a9ab1dd46e915b641a0ee935cda9468bf4
Yana <yanacommit@wizwar.net>
2020-08-02 10:00:05 -0700

    AAA Contrast works.

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
 import { AssistantPackage } from '@sketch-hq/sketch-assistant-types'
 import { AAContrastShape } from './rules/aa-contrast-shape'
 import { AAContrastText } from './rules/aa-contrast-text'
+import { AAAContrastText } from './rules/aaa-contrast-text'
 
 const assistant: AssistantPackage = async () => {
   return {
     name: 'sketch-color-contrast',
-    rules: [AAContrastShape, AAContrastText],
+    rules: [AAContrastShape, AAContrastText, AAAContrastText],
     config: {
       rules: {
         'sketch-color-contrast/aa-contrast-shape': {
           active: true,
         },
         'sketch-color-contrast/aa-contrast-text': {
+          active: true,
+        },
+        'sketch-color-contrast/aaa-contrast-text': {
           active: true,
         },
       },
