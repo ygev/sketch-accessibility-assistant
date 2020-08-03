410b2f37f7dcef4267ec67d791747d3959c3861f
Yana <yanacommit@wizwar.net>
2020-08-03 16:49:39 -0700

    Add text justification rule.

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@ import { AssistantPackage } from '@sketch-hq/sketch-assistant-types'
 import { AAContrastShape } from './rules/aa-contrast-shape'
 import { AAContrastText } from './rules/aa-contrast-text'
 import { AAAContrastText } from './rules/aaa-contrast-text'
+import { textNotJustified } from './rules/text-not-justified'
 
 const assistant: AssistantPackage = async () => {
   return {
     name: 'sketch-color-contrast',
-    rules: [AAContrastText, AAAContrastText, AAContrastShape],
+    rules: [AAContrastText, AAAContrastText, AAContrastShape, textNotJustified],
     config: {
       rules: {
         'sketch-color-contrast/aa-contrast-text': {
@@ -16,6 +17,9 @@ const assistant: AssistantPackage = async () => {
           active: true,
         },
         'sketch-color-contrast/aa-contrast-shape': {
+          active: true,
+        },
+        'sketch-color-contrast/text-not-justified': {
           active: true,
         },
       },
---
+++ src/rules/text-not-justified/index.ts
@@ -0,0 +1,20 @@
+import { RuleDefinition } from '@sketch-hq/sketch-assistant-types'
+
+export const textNotJustified: RuleDefinition = {
+  rule: async (context) => {
+    const { utils } = context
+    for (const layer of utils.objects.text) {
+      var alignment = layer.style?.textStyle?.encodedAttributes?.paragraphStyle?.alignment
+      if (alignment == undefined) {
+        continue
+      }
+      if (alignment == 3) {
+        utils.report(`Layer "${layer.name}" has justified text.`, layer)
+      }
+    }
+  },
+  name: 'sketch-color-contrast/text-not-justified',
+  title: 'Text must not be justified.',
+  description:
+    'TJustification makes reading more difficult because extra space between words causes "rivers of white" making it difficult to track along a line of text, or less space between words makes it difficult to distinguish separate words.',
+}
