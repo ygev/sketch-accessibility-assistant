fc6877111f42f71c4e9872b9d977f74ce2814f7b
Yana <yanacommit@wizwar.net>
2020-08-19 22:08:00 -0700

    Fix group detection bug for AAA text.

--- src/rules/aaa-contrast-text/index.ts
+++ src/rules/aaa-contrast-text/index.ts
@@ -1,22 +1,20 @@
 import { RuleDefinition } from '@sketch-hq/sketch-assistant-types'
 import { calcLum, calcContrast } from '../../contrast'
+import { Context } from 'vm'
 
-export const AAAContrastText: RuleDefinition = {
-  rule: async (context) => {
-    const { utils } = context
-    for (const artboard of utils.objects.artboard) {
-      var maxLayers = artboard.layers.length
+function calcAAAContrastText(sketchObject: Context, utils: Context) {
+  var maxLayers = sketchObject.layers.length
       for (var i = 0; i < maxLayers; i++) {
         for (var j = 1; j < maxLayers; j++) {
-          var botXmin = artboard.layers[i].frame.x
-          var botYmin = artboard.layers[i].frame.y
-          var botXmax = artboard.layers[i].frame.x + artboard.layers[i].frame.width
-          var botYmax = artboard.layers[i].frame.y + artboard.layers[i].frame.height
+          var botXmin = sketchObject.layers[i].frame.x
+          var botYmin = sketchObject.layers[i].frame.y
+          var botXmax = sketchObject.layers[i].frame.x + sketchObject.layers[i].frame.width
+          var botYmax = sketchObject.layers[i].frame.y + sketchObject.layers[i].frame.height
 
-          var topXmin = artboard.layers[j].frame.x
-          var topYmin = artboard.layers[j].frame.y
-          var topXmax = artboard.layers[j].frame.x + artboard.layers[j].frame.width
-          var topYmax = artboard.layers[j].frame.y + artboard.layers[j].frame.height
+          var topXmin = sketchObject.layers[j].frame.x
+          var topYmin = sketchObject.layers[j].frame.y
+          var topXmax = sketchObject.layers[j].frame.x + sketchObject.layers[j].frame.width
+          var topYmax = sketchObject.layers[j].frame.y + sketchObject.layers[j].frame.height
 
           if (topXmin < botXmax && botXmin < topXmax && topYmin < botYmax && botYmin < topYmax) {
             var topR
@@ -24,18 +22,18 @@ export const AAAContrastText: RuleDefinition = {
             var topB
 
             // Determine if top layer is a shape or text.
-            if (artboard.layers[j].style?.textStyle?.encodedAttributes.MSAttributedStringColorAttribute?.red !== undefined) {
-              topR = artboard.layers[j].style?.textStyle?.encodedAttributes.MSAttributedStringColorAttribute?.red
-              topG = artboard.layers[j].style?.textStyle?.encodedAttributes.MSAttributedStringColorAttribute?.green
-              topB = artboard.layers[j].style?.textStyle?.encodedAttributes.MSAttributedStringColorAttribute?.blue
-            } else if (artboard.layers[j].style?.fills?.[0]?.color?.red !== undefined) {
+            if (sketchObject.layers[j].style?.textStyle?.encodedAttributes.MSAttributedStringColorAttribute?.red !== undefined) {
+              topR = sketchObject.layers[j].style?.textStyle?.encodedAttributes.MSAttributedStringColorAttribute?.red
+              topG = sketchObject.layers[j].style?.textStyle?.encodedAttributes.MSAttributedStringColorAttribute?.green
+              topB = sketchObject.layers[j].style?.textStyle?.encodedAttributes.MSAttributedStringColorAttribute?.blue
+            } else if (sketchObject.layers[j].style?.fills?.[0]?.color?.red !== undefined) {
               continue
             }
 
             // In any case, the bottom is a shape.
-            var botR = artboard.layers[i].style?.fills?.[0]?.color?.red
-            var botG = artboard.layers[i].style?.fills?.[0]?.color?.green
-            var botB = artboard.layers[i].style?.fills?.[0]?.color?.blue
+            var botR = sketchObject.layers[i].style?.fills?.[0]?.color?.red
+            var botG = sketchObject.layers[i].style?.fills?.[0]?.color?.green
+            var botB = sketchObject.layers[i].style?.fills?.[0]?.color?.blue
 
             // Don't crash if something is undefined.
             if (topR == undefined || topG == undefined || topB == undefined || botR == undefined || botG == undefined || botB == undefined) {
@@ -47,7 +45,7 @@ export const AAAContrastText: RuleDefinition = {
             var botLum = calcLum(botR, botG, botB)
             var ratio = calcContrast(topLum, botLum)
 
-            var txtSize = artboard.layers[j].style?.textStyle?.encodedAttributes.MSAttributedStringFontAttribute.attributes.size
+            var txtSize = sketchObject.layers[j].style?.textStyle?.encodedAttributes.MSAttributedStringFontAttribute.attributes.size
             if (txtSize == undefined) {
               continue
             }
@@ -61,11 +59,21 @@ export const AAAContrastText: RuleDefinition = {
               continue
             } else {
               // in other cases, AAA fail
-              utils.report(`Layers ${artboard.layers[i].name} and ${artboard.layers[j].name} do not pass WCAG 2.1 AAA, and their contrast ratio is: ${ratio.toFixed(2)}:1`, artboard.layers[j])
+              utils.report(`Layers ${sketchObject.layers[i].name} and ${sketchObject.layers[j].name} do not pass WCAG 2.1 AAA, and their contrast ratio is: ${ratio.toFixed(2)}:1`, sketchObject.layers[j])
             }
           }
         }
       }
+}
+
+export const AAAContrastText: RuleDefinition = {
+  rule: async (context) => {
+    const { utils } = context
+    for (const artboard of utils.objects.artboard) {
+      calcAAAContrastText(artboard, utils)
+    }
+    for (const group of utils.objects.group) {
+      calcAAAContrastText(group, utils)
     }
   },
   name: 'sketch-accessibility-assistant/aaa-contrast-text',
